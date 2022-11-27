299a937361edd789c1162e397a7eafd7b8923381
Alexander Chernyaev <265195@niuitmo.ru>
2022-11-27 21:29:04 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const sliderMain = new Swiper('.slider__main', {
+    freeMode: true,
+    centeredSlides: true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        680: {
+            slidesPerView: 3.5,
+            spaceBetween: 60
+        }
+    }
+
+})
